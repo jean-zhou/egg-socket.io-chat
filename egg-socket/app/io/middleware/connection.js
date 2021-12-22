d016f3dcd51108e95be6a9e944bfd74bdcf005ca
jean-zhou <7774846+jean-zhou@user.noreply.gitee.com>
2021-12-22 19:33:55 +0800

    前端添加socket.io库

--- egg-socket/app/io/middleware/connection.js
+++ egg-socket/app/io/middleware/connection.js
@@ -1,9 +1,11 @@
+'use strict';
+
 // 每一个客户端链接或者退出时的操作，demo 仅打印链接
-module.exports = app => {
+module.exports = () => {
   return async (ctx, next) => {
-    ctx.socket.emit('res', 'connected')
-    await next()
+    ctx.socket.emit('res', 'connected');
+    await next();
     // 退出时
-    console.log('disconnection')
-  }
-}
+    console.log('disconnection');
+  };
+};
