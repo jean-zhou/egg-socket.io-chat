7e746244106ca7f895893614598ad074df0010a9
jean-zhou <7774846+jean-zhou@user.noreply.gitee.com>
2021-12-22 20:30:23 +0800

    补充提交

--- client/client.js
+++ client/client.js
@@ -3,6 +3,10 @@
 
   const socket = io.connect('http://127.0.0.1:7001/chat')
   console.log('socket ----', socket)
+  socket.on('connect', () => {
+    console.log('如果链接成功则打印 ----')
+    socket.emit('chat', 'hello connected')
+  })
   // 前端没有链接成功
   socket.on('res', msg => {
     console.log(`res from server${msg}`)
